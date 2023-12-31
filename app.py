c465f7d3989ba1a09368fa9c897ea023caf4a3f2
obogeanu2712 <obogeanumateirazvan@gmail.com>
2023-12-31 17:01:14 +0200

    removed design

--- app.py
+++ app.py
@@ -2,36 +2,33 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    if request.method == 'POST':
-        weight = float(request.form['weight'])
-        height = float(request.form['height'])
-
-        # BMI calculation
-        bmi = calculate_bmi(weight, height)
-        bmi_category = interpret_bmi(bmi)
-
-        return render_template('result.html', bmi=bmi, category=bmi_category)
-
 def calculate_bmi(weight, height):
-    # BMI calculation formula
-    return round((weight / (height ** 2)), 2)
+    height_in_meters = height / 100  # convert height from cm to meters
+    bmi = weight / (height_in_meters ** 2)
+    return round(bmi, 2)
 
-def interpret_bmi(bmi):
-    # Interpretation of BMI categories
+def get_weight_category(bmi):
     if bmi < 18.5:
         return 'Underweight'
-    elif 18.5 <= bmi < 24.9:
-        return 'Normal Weight'
-    elif 25 <= bmi < 29.9:
+    elif 18.5 <= bmi < 25:
+        return 'Normal weight'
+    elif 25 <= bmi < 30:
         return 'Overweight'
     else:
         return 'Obese'
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    bmi = None
+    category = None
+
+    if request.method == 'POST':
+        weight = float(request.form['weight'])
+        height = float(request.form['height'])
+        bmi = calculate_bmi(weight, height)
+        category = get_weight_category(bmi)
+
+    return render_template('index.html', bmi=bmi, category=category)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
